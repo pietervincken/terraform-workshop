03b1bc9f90fffe5e80b40a4f6a8de886d2602b4a
pietervincken <4153479+pietervincken@users.noreply.github.com>
2022-03-04 10:51:48 +0100

    WIP. Preparation for workshop

--- prepare/main.tf
+++ prepare/main.tf
@@ -9,7 +9,24 @@ locals {
   rg       = "rg-terraform-workshop"
   participants = {
     pivi = "pieter.vincken@ordina.be"
+    yabo = "yannick.bontemps@ordina.be"
+    duca = "duncan.casteleyn@ordina.be"
+    kego = "kevin.govaerts@ordina.be"
+    jeve = "jeroen.vereecken@ordina.be"
+    jast = "jago.staes@ordina.be"
+    nija = "niels.jacquet@ordina.be"
+    kolu = "koen.luyten@ordina.be"
+    olhe = "olivier.hermans@ordina.be"
+    kabo = "katrien.boesmans@ordina.be"
+    gesi = "gertjan.silkens@ordina.be"
+    chde = "chris.debruyne@ordina.be"
+    miva = "michiel.vanreybrouck@ordina.be"
     maca = "maarten.casteels@ordina.be"
+    suqu = "sujith.quintelier@ordina.be"
+    joli = "jorrit.linnert@ordina.nl"
+    frbo = "frederick.bousson@ordina.be"
+    nihe = "nick.heymans@ordina.be"
+    sava = "sam.vancutsem@ordina.be"
   }
 }
 
